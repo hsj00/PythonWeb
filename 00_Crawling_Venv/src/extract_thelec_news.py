e1453ca742efb9d3d2495e4525e972af3233c802
Peter <2.s.koushi@gmail.com>
2020-08-20 15:31:20 +0900

    200820, "code comment update"

--- 00_Crawling_Venv/src/extract_thelec_news.py
+++ 00_Crawling_Venv/src/extract_thelec_news.py
@@ -4,36 +4,53 @@
 import selenium
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
 
-THELEC_NEWS_URL = "http://www.thelec.kr/news/articleList.html"
+# from pprint import pprint
+
 # http://www.thelec.kr/news/articleList.html?page=2&total=5131&box_idxno=
 
 
 def extThelecNews():
+    # thelec.kr 전체 기사 목록을 읽어서 `html.parser`로 파싱
+    THELEC_NEWS_URL = "http://www.thelec.kr/news/articleList.html"
     thelec_results = requests.get(THELEC_NEWS_URL)
     thelec_soup = BeautifulSoup(thelec_results.text, "html.parser")
 
+    # 파싱된 데이터에서 전체 기사수 항목에서 값을 불러와 정수로 변환해서 출력
     total_articles = thelec_soup.find("small", {"class": "text-muted"}).string
     total_articles_number = int(total_articles[1:-2].replace(",", ""))
     print(f"total={total_articles_number}")
 
+    # 파싱된 데이터에서 `class`가 `pagination text-center`인 `ul` 태그? 항목을 찾아서 `a` 태그 항목 전체 검색
     pagination = thelec_soup.find("ul", {"class": "pagination text-center"})
     links = pagination.find_all("a")
 
+    # 빈 리스트를 `pages` 변수에 선언하고 `links` 변수에 선언된 데이터에서 `string` 항목들을 정수로 변환하여 추가
     pages = []
     for link in links[1:10]:
         pages.append(int(link.string))
 
+    # `pages` 리스트에서 정수로 변환된 페이지중 가장 큰 값을 반환하여 선언
     max_page = max(pages)
 
+    # 전체 페이지만큼의 값을 갖는 범위를 선언하여 `for` 루프로 "page=" 값을 출력하는 내용
+    # "page=" 값을 `page_url_list`에 선언된 리스트로 반환
+    page_url_list = []
+    for n in range(max_page):
+        page_url_str = f"page={n + 1}"
+        page_url_list.append(page_url_str)
+        # print(f"page={n + 1}")
+        print(page_url_list)
+
+    # 한 페이지에 뜨는 기사의 수를 알기 위해서 `class`가 `table-row`인 `div` 태그? 항목을 찾고 그 값의 길이를 반환하여 선언
     articles = thelec_soup.find_all("div", {"class": "table-row"})
     articles_number = len(articles)
 
-    for n in range(max_page):
-        print(f"page={n + 1}")
+    # print(articles_number)
 
-    pprint(articles_number)
+
+def thelecPageURL():
+    pass
 
 
 if __name__ == "__main__":
